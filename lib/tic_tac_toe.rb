2e58c014698676db0616e4e9e7540ff73a49c105
Justin Phillips <justinphil02@yahoo.com>
2017-09-04 00:59:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ def won?(board)
   winning_combination = WIN_COMBINATIONS.detect do |combo|
     (board[combo[0]] == "X" || board[combo[0]] == "O") && board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]]
   end
-  
+
   if winning_combination != nil
     winner = board[winning_combination[0]]
   else
